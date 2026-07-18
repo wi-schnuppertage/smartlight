ab98d38fd448a9552a7409e0db64877eb03f8940
Andreas Eckhart <andreas.eckhart@htlinn.ac.at>
2026-07-18 11:15:27 +0200

    added new project installer

--- WI-Installer.cmd
+++ WI-Installer.cmd
@@ -4,22 +4,16 @@ setlocal
 :: Pfad zum Dokumente-Verzeichnis via Windows API ermitteln
 for /f "delims=" %%I in ('powershell -NoProfile -Command "[Environment]::GetFolderPath('MyDocuments')"') do set "DOCS_DIR=%%I"
 
-:: Jetzt sicher in den Ordner wechseln
-cd /d "%DOCS_DIR%\DeinRepoName"
-break
-
 :: Pfad zum Arduino-Ordner definieren
 set "PROJECT_DIR=WI-Schnuppertag"
-set "TARGET_DIR=%USERPROFILE%\Documents\Arduino\%TARGET_DIR%"
+set "TARGET_DIR=%DOCS_DIR%\Arduino\%PROJECT_DIR%"
+
 :: HIER DIE REPO-URL ANPASSEN
 set "REPO_URL=https://github.com/wi-schnuppertage/smartlight.git"
 
 :: HIER DEN NAMEN DEINER SKETCH-DATEI ANPASSEN (inkl. .ino)
 :: Falls sie direkt im Hauptverzeichnis des Repos liegt, z.B. "MeinProjekt.ino"
 set "SKETCH_NAME=01_Programmieren\01_Programmieren.ino"
-
-echo %TARGET_DIR%
-echo %SKETCH_NAME%
 
 echo ===================================================
 echo Starte Repository-Aktualisierung...
@@ -32,29 +26,33 @@ if exist "%TARGET_DIR%" (
 )
 
 :: 2. In den Arduino-Mutterordner wechseln
-if not exist "%USERPROFILE%\Documents\Arduino" (
-    mkdir "%USERPROFILE%\Documents\Arduino"
+if not exist "%DOCS_DIR%\Arduino" (
+    mkdir "%DOCS_DIR%\Arduino"
 )
-cd /d "%USERPROFILE%\Documents\Arduino"
+cd /d "%DOCS_DIR%\Arduino"
 
 :: 3. Repo frisch klonen
 echo Klone Repository neu...
 git clone "%REPO_URL%" %PROJECT_DIR%
 
 if %ERRORLEVEL% equ 0 (
+    for /l %%i in (1,1,5) do (
+    cls
+	color 0A
     echo.
-    echo ===================================================
-    echo Erfolg: Repository wurde erfolgreich aktualisiert!
-    echo ===================================================
-    
-    :: 4. Arduino IDE mit dem Sketch starten
-    echo Starte Arduino IDE...
-    if exist "PROJECT_DIR\01_Programmieren\%SKETCH_NAME%" (
-        start "" "PROJECT_DIR\%SKETCH_NAME%"
-    ) else (
-        echo HINWEIS: Die Datei %SKETCH_NAME% wurde im Hauptordner des Repos nicht gefunden.
-    )
-    
+    echo =================================================================
+    echo -  INSTALLATION ERFOLGREICH!
+    echo -  -------------------------------------------------------------
+    echo -  Projekt wurde erfolgreich installiert.
+    echo.
+	echo -  Starte nun die Arduino-IDE um zu beginnen.
+    echo =================================================================
+    echo.
+	timeout /t 1 >nul
+	color 07
+    timeout /t 1 >nul
+	)
+
 ) else (
     echo.
     echo ---------------------------------------------------
@@ -63,6 +61,4 @@ if %ERRORLEVEL% equ 0 (
     echo ---------------------------------------------------
 )
 
-:: Ein kurzes Timeout statt "pause", damit sich das Fenster 
-:: nach 3 Sekunden von selbst schliesst, wenn alles geklappt hat
 timeout /t 3
